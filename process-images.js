f7821b909a44850ed6770c45324b96b137180545
GitHub Action <action@github.com>
2025-05-23 17:56:34 +0000

    Generate responsive images for deployment

--- process-images.js
+++ process-images.js
@@ -23,28 +23,19 @@ if (fs.existsSync(sourceDir)) {
 
     // Create small version (480px width)
     sharp(filePath)
-      .resize(480, 480, {
-        fit: 'cover',
-        position: 'center'
-      })
+      .resize(480)
       .toFile(path.join(outputDir, `${fileNameWithoutExt}-small${fileExt}`))
       .catch(err => console.error(`Error processing ${file} for small size:`, err));
 
     // Create medium version (768px width)
     sharp(filePath)
-      .resize(768, 768, {
-        fit: 'cover',
-        position: 'center'
-      })
+      .resize(768)
       .toFile(path.join(outputDir, `${fileNameWithoutExt}-medium${fileExt}`))
       .catch(err => console.error(`Error processing ${file} for medium size:`, err));
 
     // Create large version (1200px width)
     sharp(filePath)
-      .resize(1200, 1200, {
-        fit: 'cover',
-        position: 'center'
-      })
+      .resize(1200)
       .toFile(path.join(outputDir, `${fileNameWithoutExt}-large${fileExt}`))
       .catch(err => console.error(`Error processing ${file} for large size:`, err));
 
